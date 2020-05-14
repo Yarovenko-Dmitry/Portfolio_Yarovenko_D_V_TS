2c781d765f3b816a7f2cfe28e2df435e308dd9a3
prostiak2019 <yarovenkodmv@gmail.com>
2020-05-14 15:00:30 +0300

    Updates

---
+++ precache-manifest.d1f0fbab85b9fd3c19840636222eaa2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e478691a46f00d49bfde5e8b3a45bd4c",
+    "url": "/Portfolio_Yarovenko_D_V_TS/index.html"
+  },
+  {
+    "revision": "629d0c2f5c4c6d5e53a2",
+    "url": "/Portfolio_Yarovenko_D_V_TS/static/css/main.e24a1d9f.chunk.css"
+  },
+  {
+    "revision": "d5c49fbe5ef9ad52f9f1",
+    "url": "/Portfolio_Yarovenko_D_V_TS/static/js/2.939e5ce0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio_Yarovenko_D_V_TS/static/js/2.939e5ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629d0c2f5c4c6d5e53a2",
+    "url": "/Portfolio_Yarovenko_D_V_TS/static/js/main.b0adb530.chunk.js"
+  },
+  {
+    "revision": "6250f47aee92af6b6ae0",
+    "url": "/Portfolio_Yarovenko_D_V_TS/static/js/runtime-main.1f75fd6a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3161c2eea30ac420656f2e993473502c.js"
+  "/Portfolio_Yarovenko_D_V_TS/precache-manifest.d1f0fbab85b9fd3c19840636222eaa2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio_Yarovenko_D_V_TS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
